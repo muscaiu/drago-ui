14a24050acb17b990933918b3b4da7d364c54e37
muscaiu <muscaiu@yahoo.com>
2024-05-21 15:46:49 +0300

    export codeblock

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Drago-UI v0.3.8
+# Drago-UI v0.3.9
  
 Demo: https://6647358832ebdbe9a0ba8e06-puymcsjqwx.chromatic.com
 
--- lib/main.ts
+++ lib/main.ts
@@ -2,3 +2,4 @@ import './tailwind.css';
 
 export { Button } from './Button/Button';
 export { Input } from './Input/Input';
+export { CodeBlock } from './CodeBlock/CodeBlock';
